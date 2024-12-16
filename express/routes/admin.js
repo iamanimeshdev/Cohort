a4b300866934e011660e3a03a8afb581793dcc8c
iamanimeshdev <iamanimeshdev@gmail.com>
2024-12-16 21:47:01 +0530

    added db.js

--- express/routes/admin.js
+++ express/routes/admin.js
@@ -1,12 +1,33 @@
 const {Router}=require("express");
 const adminRouter=Router();
+const {userModel,adminModel,courseModel,purchaseModel}=require("../db")
 
 adminRouter.post("/signup",(req,res) =>{
-    res.json("SignUp")
+    const {email, password,firstName,lastName}=req.body;
+
+    adminModel.create({
+        email:email,
+        password:password,
+        firstName:firstName,
+        lastName:lastName
+
+    })
+    res.json({
+        message:"SignUp Completed"
+    })
 
 })
 adminRouter.post("/signin",(req,res) =>{
-    res.json("Signin")
+    const {email, password}=req.body;
+
+   const admin= adminModel.findOne({
+        email:email,
+        password:password
+    })
+    if(admin)
+    {
+        res.json({message:"you are logged in"})
+    }
 
 })
 adminRouter.put("/course",(req,res) =>{
